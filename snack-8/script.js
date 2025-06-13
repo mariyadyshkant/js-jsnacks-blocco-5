d27b8d9691af60611634d1efa4d76cacba5e4619
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-13 17:55:38 +0200

    Add functionality to retrieve and log the class of a specific student in snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studentClass = students.find(person => person.name === "Marco Lanci");
+console.log(studentClass);
+console.log(studentClass.class);
